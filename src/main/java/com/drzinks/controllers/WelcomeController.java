212f279f2193a2542e5415775fc2b4e03156feab
Golebiowski <michal.golebiowski@hpe.com>
2016-09-05 12:08:30 +0200

    Spring MVC first view

--- src/main/java/com/drzinks/controllers/WelcomeController.java
+++ src/main/java/com/drzinks/controllers/WelcomeController.java
@@ -4,6 +4,7 @@ import com.drzinks.dtos.WelcomeDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by golebiow on 2016-03-10.
@@ -11,12 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/welcome")
-    public @ResponseBody WelcomeDTO returnWelcomeView() {
-        WelcomeDTO welcomeDTO = new WelcomeDTO();
-        welcomeDTO.setWelcomeMessage("welc message");
-        welcomeDTO.setTitle("tytul");
-        return welcomeDTO;
-    }
+//    @RequestMapping("/welcome")
+//    public @ResponseBody WelcomeDTO returnWelcomeView() {
+//        WelcomeDTO welcomeDTO = new WelcomeDTO();
+//        welcomeDTO.setWelcomeMessage("welc message");
+//        welcomeDTO.setTitle("tytul");
+//        return welcomeDTO;
+//    }
+
+
+  @RequestMapping(value="/")
+  public ModelAndView returnWelcomeView(ModelAndView model) {
+      model.setViewName("home");
+      return model;
+  }
 
 }
