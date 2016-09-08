54a6bff04c4c9aa5a59f1752a9fcbd949ec004c7
Golebiowski <michal.golebiowski@hpe.com>
2016-09-08 15:26:46 +0200

    restucturised spring conf acc to whts in kobietydokodu.pl

---
+++ src/main/java/com/drzinks/dtos/ContactDTO.java
@@ -0,0 +1,15 @@
+package com.drzinks.dtos;
+
+
+public class ContactDTO {
+
+	
+	private int id;
+	
+	
+	private String name;
+/*    <td>${contact.name}</td>
+    <td>${contact.email}</td>
+    <td>${contact.phone}</td>*/
+	
+}
